9ae933be58382e913c67a2bbec7007dc1e50c33a
Aashutosh Pudasaini <nepalidude3@gmail.com>
2025-04-27 23:20:46 +0545

    add: lecture

--- data/seeder.go
+++ data/seeder.go
@@ -18,34 +18,61 @@ func SeedVideos(videos []*Video, DB *gorm.DB) error {
 func seedVideo(video *Video, db *gorm.DB) error {
 	class := getClass(video.Class, db)
 	teacher := getTeacher(video.TeacherName, db)
+	subject := getSubject(class.ID, video.SubjectType, video.SubjectName, db)
+	// lecture := getLecture()
 
 	fmt.Println("got class : ", *class)
 	fmt.Println("got teacher : ", *teacher)
+	fmt.Println("got subject: ", *subject)
 
 	return nil
 }
 
 func getClass(classNumber int, DB *gorm.DB) *db.Class {
-	var class db.Class
 
+	var class db.Class
 	result := DB.Where("number = ?", classNumber).First(&class)
 
 	if result.Error == gorm.ErrRecordNotFound {
 		class = db.Class{Number: classNumber}
 		DB.Create(&class)
+
 	}
 
 	return &class
 }
 
 func getTeacher(teacherName string, DB *gorm.DB) *db.Teacher {
-	var teacher db.Teacher
 
+	var teacher db.Teacher
 	result := DB.Where("name = ?", teacherName).First(&teacher)
 
 	if result.Error == gorm.ErrRecordNotFound {
+
 		teacher = db.Teacher{Name: teacherName}
 		DB.Create(&teacher)
+
 	}
+
 	return &teacher
+}
+
+func getSubject(classId uint, subjectType db.SubjectType, subjectName string, DB *gorm.DB) *db.Subject {
+
+	var subject db.Subject
+	result := DB.Where("name = ? AND type = ?", subjectName, subjectType).First(&subject)
+
+	if result.Error == gorm.ErrRecordNotFound {
+
+		subject = db.Subject{
+			Name:    subjectName,
+			Type:    subjectType,
+			ClassId: classId,
+		}
+
+		DB.Create(&subject)
+
+	}
+
+	return &subject
 }
