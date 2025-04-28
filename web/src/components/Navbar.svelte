a833c327134dbd698246f00b370f29e7bf75b27b
chrisdvkta <chrisdevkota77@gmail.com>
2025-04-28 17:21:24 +0545

    add: navbar

---
+++ web/src/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import logo from '$lib/assets/dlc.png';
+</script>
+
+<nav class="flex flex-row items-center justify-between p-5">
+	<a href="/">
+		<img alt="Deerwalk Learning center" src={logo} width="200px" height="200px" />
+	</a>
+	<div class=" flex gap-5 text-black">
+		<a href="/" class="hover:text-gray-800">Classes</a>
+		<a href="/teachers" class="hover:text-gray-800">Teachers</a>
+	</div>
+</nav>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	export const prerender = true;
+	import Navbar from '$components/Navbar.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex flex-col">
+	<Navbar />
+	<div class="flex-1">
+		{@render children()}
+	</div>
+</div>
