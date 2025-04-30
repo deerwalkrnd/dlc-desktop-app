4d02c75c5759a20769f6be02707881285c152ca2
chrisdvkta <chrisdevkota77@gmail.com>
2025-04-30 17:11:18 +0545

    update:error page

--- web/src/routes/class/[classId]/+error.svelte
+++ web/src/routes/class/[classId]/+error.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import ErrorSVG from '$lib/assets/alert.svg';
 	import Dog404 from '$lib/assets/animation.webm';
+	import Cat404 from '$lib/assets/animation2.webm';
 </script>
 
-<div class="flex h-[100vh] flex-col items-center justify-center">
+<div class="flex h-[80vh] flex-col items-center justify-center">
 	<div class="flex flex-col items-center justify-center gap-5 text-red-900">
 		<div>
-			<video src={Dog404} loop autoplay> <track kind="captions" /></video>
+			<video src={Cat404} loop autoplay> <track kind="captions" /></video>
+			<!-- <video src={Dog404} loop autoplay> <track kind="captions" /></video> -->
 		</div>
 		<div class="text-4xl font-[700]">Class not found : /</div>
 		<div class="cursor-pointer text-xl">
