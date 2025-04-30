89243e3909f06d4145f9fc2f025fdf1c2079c213
chrisdvkta <chrisdevkota77@gmail.com>
2025-04-30 17:05:29 +0545

    add:error 404 page

---
+++ web/src/routes/class/[classId]/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import ErrorSVG from '$lib/assets/alert.svg';
+	import Dog404 from '$lib/assets/animation.webm';
+</script>
+
+<div class="flex h-[100vh] flex-col items-center justify-center">
+	<div class="flex flex-col items-center justify-center gap-5 text-red-900">
+		<div>
+			<video src={Dog404} loop autoplay> <track kind="captions" /></video>
+		</div>
+		<div class="text-4xl font-[700]">Class not found : /</div>
+		<div class="cursor-pointer text-xl">
+			Go back <span class="underline"><a aria-label="error" href="/"> Home</a> </span>
+		</div>
+	</div>
+</div>
